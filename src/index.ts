6bf73465824068be453b1d19feb3d1ec9fbed769
tomsdoo <32782986+tomsdoo@users.noreply.github.com>
2022-10-01 06:40:52 +0900

    husky is added (#32)
    * husky is installed
    
    * npm script prepare is added
    
    * pre-commit format
    
    * format

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,10 @@ const { ipcRenderer } = require("electron");
 export const handles = [
   {
     eventName: "electronade-filestore:get",
-    handler: async (event: any, { filePath, id }: { filePath: string; id: string }) =>
-      await new FileDb(filePath).get(id),
+    handler: async (
+      event: any,
+      { filePath, id }: { filePath: string; id: string }
+    ) => await new FileDb(filePath).get(id),
   },
   {
     eventName: "electronade-filestore:getids",
@@ -22,8 +24,10 @@ export const handles = [
   },
   {
     eventName: "electronade-filestore:remove",
-    handler: async (event: any, { filePath, id }: { filePath: string; id: string }) =>
-      await new FileDb(filePath).remove(id),
+    handler: async (
+      event: any,
+      { filePath, id }: { filePath: string; id: string }
+    ) => await new FileDb(filePath).remove(id),
   },
 ];
 
@@ -34,8 +38,14 @@ export const preloadObject = {
     getIds: async (filePath: string) =>
       await ipcRenderer.invoke("electronade-filestore:getids", { filePath }),
     save: async (filePath: string, item: object) =>
-      await ipcRenderer.invoke("electronade-filestore:save", { filePath, item }),
+      await ipcRenderer.invoke("electronade-filestore:save", {
+        filePath,
+        item,
+      }),
     remove: async (filePath: string, id: string) =>
-      await ipcRenderer.invoke("electronade-filestore:remove", { filePath, id }),
+      await ipcRenderer.invoke("electronade-filestore:remove", {
+        filePath,
+        id,
+      }),
   },
 };
