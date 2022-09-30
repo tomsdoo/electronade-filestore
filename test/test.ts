78b30099f519a0466636ba17b1e7ea2cc885a865
tom <toms@live.jp>
2022-10-01 06:18:32 +0900

    format:test

--- test/test.ts
+++ test/test.ts
@@ -6,7 +6,7 @@ import { handles, preloadObject } from "../src/";
 const ipcRenderer: {
   invoke: (eventName: string, ...args: any[]) => Promise<any>;
 } = {
-  invoke: (eventName: string) => Promise.resolve(eventName)
+  invoke: (eventName: string) => Promise.resolve(eventName),
 };
 
 let handleStore: {
@@ -16,40 +16,42 @@ let handleStore: {
 describe("preloadObject to handles", () => {
   before(() => {
     handleStore = Object.fromEntries(
-      handles.map(({ eventName, handler }) =>
-        [eventName, handler ])
+      handles.map(({ eventName, handler }) => [eventName, handler])
     );
   });
 
   it("electronade-filestore:get", async () => {
     assert(
-      await eval(preloadObject.filestore.get.toString())
-        ("dummyFilePath", "dummyId")
-        in handleStore
+      (await eval(preloadObject.filestore.get.toString())(
+        "dummyFilePath",
+        "dummyId"
+      )) in handleStore
     );
   });
 
   it("electronade-filestore:getids", async () => {
     assert(
-      await eval(preloadObject.filestore.getIds.toString())
-        ("dummyFilePath")
-        in handleStore
+      (await eval(preloadObject.filestore.getIds.toString())(
+        "dummyFilePath"
+      )) in handleStore
     );
   });
 
   it("electronade-filestore:save", async () => {
     assert(
-      await eval(preloadObject.filestore.save.toString())
-        ("dummyFilePath", {})
-        in handleStore
+      (await eval(preloadObject.filestore.save.toString())(
+        "dummyFilePath",
+        {}
+      )) in handleStore
     );
   });
 
   it("electronade-filestore:remove", async () => {
     assert(
-      await eval(preloadObject.filestore.remove.toString())
-        ("dummyFilePath", "dummyId")
-        in handleStore
+      (await eval(preloadObject.filestore.remove.toString())(
+        "dummyFilePath",
+        "dummyId"
+      )) in handleStore
     );
   });
 });
