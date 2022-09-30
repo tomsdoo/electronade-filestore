a8578081c0b258cfd669c6d939bec5b45c6c217a
tomsdoo <32782986+tomsdoo@users.noreply.github.com>
2022-10-01 06:35:07 +0900

    eslint is added (#31)
    * eslint is installed
    
    * npm script lint:src is added
    
    * lint:src fix
    
    * npm script lint:test is added
    
    * lint:test fix

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,41 @@
 import { FileDb } from "@tomsd/fsdb";
+// eslint-disable-next-line  @typescript-eslint/no-var-requires
 const { ipcRenderer } = require("electron");
 
 export const handles = [
   {
     eventName: "electronade-filestore:get",
-    handler: (event: any, { filePath, id }: { filePath: string; id: string }) =>
-      new FileDb(filePath).get(id),
+    handler: async (event: any, { filePath, id }: { filePath: string; id: string }) =>
+      await new FileDb(filePath).get(id),
   },
   {
     eventName: "electronade-filestore:getids",
-    handler: (event: any, { filePath }: { filePath: string }) =>
-      new FileDb(filePath).getIds(),
+    handler: async (event: any, { filePath }: { filePath: string }) =>
+      await new FileDb(filePath).getIds(),
   },
   {
     eventName: "electronade-filestore:save",
-    handler: (
+    handler: async (
       event: any,
       { filePath, item }: { filePath: string; item: object }
-    ) => new FileDb(filePath).save(item),
+    ) => await new FileDb(filePath).save(item),
   },
   {
     eventName: "electronade-filestore:remove",
-    handler: (event: any, { filePath, id }: { filePath: string; id: string }) =>
-      new FileDb(filePath).remove(id),
+    handler: async (event: any, { filePath, id }: { filePath: string; id: string }) =>
+      await new FileDb(filePath).remove(id),
   },
 ];
 
 export const preloadObject = {
   filestore: {
-    get: (filePath: string, id: string) =>
-      ipcRenderer.invoke("electronade-filestore:get", { filePath, id }),
-    getIds: (filePath: string) =>
-      ipcRenderer.invoke("electronade-filestore:getids", { filePath }),
-    save: (filePath: string, item: object) =>
-      ipcRenderer.invoke("electronade-filestore:save", { filePath, item }),
-    remove: (filePath: string, id: string) =>
-      ipcRenderer.invoke("electronade-filestore:remove", { filePath, id }),
+    get: async (filePath: string, id: string) =>
+      await ipcRenderer.invoke("electronade-filestore:get", { filePath, id }),
+    getIds: async (filePath: string) =>
+      await ipcRenderer.invoke("electronade-filestore:getids", { filePath }),
+    save: async (filePath: string, item: object) =>
+      await ipcRenderer.invoke("electronade-filestore:save", { filePath, item }),
+    remove: async (filePath: string, id: string) =>
+      await ipcRenderer.invoke("electronade-filestore:remove", { filePath, id }),
   },
 };
